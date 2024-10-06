4a6b5c11a2274265bd10a401fa16dbf6cccd4dbb
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-10-06 20:05:02 +0530

    fix: resolve issues with word, sentence, character, and paragraph counting logic

---
+++ main.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("textarea");
+const wordCount = document.querySelector("[data-word-count]");
+const characterCount = document.querySelector("[data-character-count]");
+const sentenceCount = document.querySelector("[data-sentence-count]");
+const paragraphCount = document.querySelector("[data-paragraph-count]");
+
+function updateCounts() {
+  if (!input.value) {
+    resetCounts();
+    return;
+  }
+
+  const wordsArray = input.value.split(" ").filter((word) => word !== "");
+  wordCount.innerText = wordsArray.length;
+
+  characterCount.innerText = input.value.length;
+
+  const sentenceArray = input.value.split(/[./]/);
+
+  sentenceCount.innerText = sentenceArray.length - 1;
+
+  const paragraphArray = input.value.split("\n").filter((p) => p.trim() !== "");
+
+  paragraphCount.innerText = paragraphArray.length;
+}
+
+function resetCounts() {
+  wordCount.textContent =
+    characterCount.textContent =
+    sentenceCount.textContent =
+    paragraphCount.textContent =
+      "0";
+}
+
+input.addEventListener("input", updateCounts);
